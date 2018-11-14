4dd5987fbc33ef1fe50b89a1cefe7e685cf69146
Unknown <patthraporn.tu@mail.kmutt.ac.th>
2018-11-15 00:36:51 +0700

    [ADD] History

---
+++ ProjectWebPro/src/java/controller/HistoryJpaController.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import controller.exceptions.NonexistentEntityException;
+import controller.exceptions.RollbackFailureException;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+import model.Customer;
+import model.History;
+
+/**
+ *
+ * @author kao-tu
+ */
+public class HistoryJpaController implements Serializable {
+
+    public HistoryJpaController(UserTransaction utx, EntityManagerFactory emf) {
+        this.utx = utx;
+        this.emf = emf;
+    }
+    private UserTransaction utx = null;
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(History history) throws RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            Customer customernumber = history.getCustomernumber();
+            if (customernumber != null) {
+                customernumber = em.getReference(customernumber.getClass(), customernumber.getCustomernumber());
+                history.setCustomernumber(customernumber);
+            }
+            em.persist(history);
+            if (customernumber != null) {
+                customernumber.getHistoryList().add(history);
+                customernumber = em.merge(customernumber);
+            }
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(History history) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            History persistentHistory = em.find(History.class, history.getHistoryid());
+            Customer customernumberOld = persistentHistory.getCustomernumber();
+            Customer customernumberNew = history.getCustomernumber();
+            if (customernumberNew != null) {
+                customernumberNew = em.getReference(customernumberNew.getClass(), customernumberNew.getCustomernumber());
+                history.setCustomernumber(customernumberNew);
+            }
+            history = em.merge(history);
+            if (customernumberOld != null && !customernumberOld.equals(customernumberNew)) {
+                customernumberOld.getHistoryList().remove(history);
+                customernumberOld = em.merge(customernumberOld);
+            }
+            if (customernumberNew != null && !customernumberNew.equals(customernumberOld)) {
+                customernumberNew.getHistoryList().add(history);
+                customernumberNew = em.merge(customernumberNew);
+            }
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = history.getHistoryid();
+                if (findHistory(id) == null) {
+                    throw new NonexistentEntityException("The history with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            History history;
+            try {
+                history = em.getReference(History.class, id);
+                history.getHistoryid();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The history with id " + id + " no longer exists.", enfe);
+            }
+            Customer customernumber = history.getCustomernumber();
+            if (customernumber != null) {
+                customernumber.getHistoryList().remove(history);
+                customernumber = em.merge(customernumber);
+            }
+            em.remove(history);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<History> findHistoryEntities() {
+        return findHistoryEntities(true, -1, -1);
+    }
+
+    public List<History> findHistoryEntities(int maxResults, int firstResult) {
+        return findHistoryEntities(false, maxResults, firstResult);
+    }
+
+    private List<History> findHistoryEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(History.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public History findHistory(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(History.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getHistoryCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<History> rt = cq.from(History.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
